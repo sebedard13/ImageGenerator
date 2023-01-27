42fff67653f54576d2c69ef03283800eb52ea9d4
Sebastien Bedard <sebedard13@gmail.com>
2023-01-27 09:05:35 -0500

    Cmake Good?

--- WorldGeneration/main.cpp
+++ WorldGeneration/main.cpp
@@ -4,11 +4,13 @@
 #include "5-controller/Controller.h"
 #include "4-domain/algos/DistanceFromCenter.h"
 #include <QtWidgets/QApplication>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
 	SetConsoleOutputCP(65001);//Console en utf-8
 	Localization::setLanguage("fr");
+    std::cout << "oui"<<std::endl;
 	QApplication a(argc, argv);
 	ViewRoot w;
 	w.show();
